c04258465635ae12a4f489aa360b6bef9e371343
AntonioManini <manini.antonio@studenti.bassi.edu.it>
2026-02-05 13:44:21 +0100

    intestazione

---
+++ headerhome.php
@@ -0,0 +1,29 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Fuoriclasse a Lodi</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="styleheader.css">
+</head>
+<body>
+
+<header class="site-header">
+    <div class="header-bar">
+
+        <a href="#" class="logo-link">
+            <img src="img/png.png" alt="Fuoriclasse a Lodi" class="logo-img">
+        </a>
+
+        <nav class="nav-menu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Chi siamo</a></li>
+                <li><a href="#">Percorsi</a></li>
+                <li><a href="#">Info</a></li>
+</body>
+</html>
+
